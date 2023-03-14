2cf786fdd494a158c1732f60c06af4b3b333b24b
jonbjarki <jonbjarki02@gmail.com>
2023-03-14 12:02:42 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import discord
+import os
+import datetime
+from discord.ext import commands,tasks
+from songs import Songs
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!',intents=intents)
+TOKEN = os.getenv("LOTD_TOKEN")
+
+songs = Songs()
+
+# Channel to spit output the Lyric of the day
+lotd_channel = 1085002849345339452
+
+# Time when to refresh LOTD
+LOTD_time = datetime.time(hour = 5, tzinfo=datetime.timezone.utc)
+
+class MyCog(commands.Cog):
+    def __init__(self,bot) -> None:
+        self.bot = bot
+        self.new_lyric.start()
+
+    def cog_unload(self) -> None:
+        self.new_lyric.cancel()
+
+    @tasks.loop(time=LOTD_time)
+    async def new_lyric(self):
+        # Outputs a new LOTD at a specific time every day
+        channel = bot.get_channel(lotd_channel)
+        lyric = songs.get_random_lyric()
+        await channel.send(f"**The Lyric Of The Day**\n\n♪ {lyric} ♪")
+
+@bot.event
+async def on_ready():
+    await bot.add_cog(MyCog(bot)) # starts listening for when to reset LOTD
+    print('Bot is ready.')
+
+@bot.command()
+@commands.has_any_role("Mods", "Lil Mods", 810115393871544350) # ID Is AK's Role
+async def lotd(ctx):
+    """
+    Resets the LOTD and outputs it
+    """
+    lyric = songs.get_random_lyric()
+    await ctx.send(f"♪ {lyric} ♪")
+
+@lotd.error
+async def lotd_error(ctx,error):
+    if isinstance(error, commands.MissingAnyRole):
+        await ctx.send("You do not have permission to use this command")
+
+@bot.command()
+async def guess(ctx, *, answer):
+    """
+    Allows a person to guess the LOTD
+    """
+    todays_song = songs.todays_song
+    if todays_song == None:
+        await ctx.send("There is no lyric of the day")
+        return
+    for song in songs.songs:
+        if song["ID"] == todays_song:
+            if answer.lower() == song["name"].lower():
+                await ctx.send(f"Correct! {ctx.author.mention} has guessed todays lyric")
+                try:
+                    await ctx.message.delete()
+                except:
+                    print("I do not have delete permissions")
+            else:
+                await ctx.send("Incorrect :( Try again!") 
+            return
+    print("Something went wrong")
+
+@guess.error
+async def guess_error(ctx,error):
+    if isinstance(error,commands.MissingRequiredArgument):
+        await ctx.send("You need to give an answer")
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
+
+
---
+++ songs.py
@@ -0,0 +1,43 @@
+import json
+import random
+jsonFile = "songs.json"
+songsDir = "lyrics/"
+
+def get_all_songs():
+    songs = []
+    with open(jsonFile,"r") as jFile:
+        songsInfo = json.load(jFile)["songs"]
+
+    for song in songsInfo:
+        with open(songsDir + song["lyricsFile"],"r") as f:
+            lines = [x for x in f.read().split("\n") if x != ""]
+            songs.append({
+                "ID": song["ID"],
+                "name": song["name"],
+                "lyrics": lines 
+            })
+
+    return songs
+
+class Songs:
+    def __init__(self) -> None:
+        self.songs = get_all_songs()
+        self.todays_song = None
+
+    def get_random_lyric(self):
+        """
+        Gets two random lines from a random song
+        """
+        self.refresh_songs()
+        song = self.songs[random.randint(0,len(self.songs)-1)]
+        songLyric = song["lyrics"]
+        lyricIndex = random.randint(0,len(songLyric)-2)
+        
+        self.todays_song = song["ID"]
+        return " ".join(songLyric[lyricIndex:lyricIndex+2])
+    
+    def refresh_songs(self):
+        """
+        Fetches all songs from files
+        """
+        self.songs = get_all_songs()
